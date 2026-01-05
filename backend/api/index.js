a9042fe6098a3ac901e4c33f813226ad1f4d0aa8
Aashray Narang <aashrayofficial7@gmail.com>
2026-01-05 15:49:48 +0530

    commit 4

--- backend/api/index.js
+++ backend/api/index.js
@@ -60,6 +60,9 @@ app.use(express.json({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 /* -------------------- ROUTES -------------------- */
 app.use("/api/auth", authRouter);
